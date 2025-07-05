87ce9aac8fa9f6a39354b3acd9b3fc49b3ce068e
ALi Hassan <alihgad2@gmail.com>
2025-07-05 17:16:04 +0300

    Update multer.js

--- src/middelWares/multer.js
+++ src/middelWares/multer.js
@@ -18,10 +18,10 @@ const storage = multer.diskStorage({})
 const fileFilter = function (req, file, cb) {
 
     const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/svg+xml', "video/mp4", "video/webm", "video/ogg", "video/3gpp", "video/3gpp2", "video/3gpp-tt"];
-    if (allowedMimeTypes.includes(file.mimetype)) {
-        return cb(null, true)
-    }
-    cb(new Error("file not supported"), 500)
+   // if (allowedMimeTypes.includes(file.mimetype)) {
+         cb(null, true)
+   // }
+   // cb(new Error("file not supported"), 500)
 }
 
 export const upload = multer({ storage, fileFilter })
