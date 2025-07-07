20787c7f511fca5622b14bc03f3db428da30c880
ALi Hassan <alihgad2@gmail.com>
2025-07-08 01:05:10 +0300

    Update section.service.js

--- src/moduels/section/section.service.js
+++ src/moduels/section/section.service.js
@@ -342,14 +342,14 @@ export let updateSlide = async (req, res, next) => {
 }
 
 export let updateSlider = async (req, res, next) => {
-console.log(typeof req.body.title , "before")
+console.log(typeof req.body.title.arabic , "before")
     if (typeof req.body?.title === 'string') {
         req.body.title = JSON.parse(req.body?.title)
     }
     if (typeof req.body?.content === 'string') {
         req.body.content = JSON.parse(req.body?.content)
     }
-    console.log(typeof req.body.title , "after")
+    console.log(typeof req.body.title.arabic , "after")
 
 
     let slider = await SliderModel.findById(req.params.sliderId)
